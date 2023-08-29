2befb17b9bff52a18166f35db19d9b3bcbf418d2
f0rbit <dev@forbit.dev>
2023-08-29 15:48:22 +0930

    move types & enums to seperate file

---
+++ src/types.ts
@@ -0,0 +1,55 @@
+/// <reference types="astro/client" />
+
+export const PROJECT_STATUS = {
+    DEVELOPMENT: "DEVELOPMENT",
+    RELEASED: "RELEASED",
+    STOPPED: "STOPPED",
+    LIVE: "LIVE",
+    FINISHED: "FINISHED",
+    PAUSED: "PAUSED",
+    ABANDONED: "ABANDONED"
+}
+
+export type PROJECT_STATUS = (typeof PROJECT_STATUS)[keyof typeof PROJECT_STATUS];
+
+export type Project = {
+    project_id: string,
+    owner_id: string,
+    name: string,
+    created_at: string,
+    updated_at: string,
+    description: string | null,
+    specification: string | null,
+    repo_url: string | null,
+    icon_url: string | null,
+    status: PROJECT_STATUS,
+    deleted: boolean,
+    link_url: string | null,
+    link_text: string | null,
+    current_version: string | null
+}
+
+export const SKILL = {
+    JAVA: "Java",
+    SQL: "SQL",
+    PHP: "PHP",
+    GAMEMAKER: "GameMaker",
+    SCRATCH: "Scratch",
+    JAVASCIPRT: "JavaScript",
+    HTML: "HTML",
+    CSS: "CSS",
+    LEADERSHIP: "Leadership",
+    DEPLOYMENT: "Deployment"
+}
+
+export const SKILL_EVENT_TYPE = {
+    BEGIN: "BEGIN",
+    LEARN: "LEARN",
+    MASTER: "MASTER"
+}
+
+export type Skill = (typeof SKILL)[keyof typeof SKILL]
+
+export type SkillEventType = (typeof SKILL_EVENT_TYPE)[keyof typeof SKILL_EVENT_TYPE];
+
+export type SkillEvent = { skill: Skill, events: SkillEventType[] };
