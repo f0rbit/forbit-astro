51b73bea62fc521f74bd0ab37b43e81498228bd1
f0rbit <dev@forbit.dev>
2024-03-07 22:52:45 +1030

    publish time will use client timezone

---
+++ src/components/blog/PublishTime.tsx
@@ -0,0 +1,29 @@
+import moment from "moment";
+
+interface Props {
+    date: string
+}
+
+export default function PublishTime(props: Props) {
+    const { date } = props;
+    // if the date was withing the past 2 days we want to use fromNow()
+    // other wise calendar({ sameElse: "DD/MM/yyyy" }) will be used
+    
+    let time = null;
+    if (moment(date).isAfter(moment().subtract(2, "days"))) {
+        time = moment(date).fromNow();
+    } else {
+        time = moment(date).calendar({ sameElse: "DD/MM/yyyy" });
+    }
+
+    const title = moment(date).format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+
+
+    return (
+        <>
+        <span className="text-base-text-subtle">Published </span>
+        <time dateTime={date} className="text-base-text-subtle lowercase" title={title}>{time}</time>
+        </>
+    )
+}
