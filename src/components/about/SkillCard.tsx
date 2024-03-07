b875f9014e5bc903abe79715584ff2852bab2e8a
f0rbit <dev@forbit.dev>
2024-03-07 19:18:15 +1030

    fix skill collector on small screen

--- src/components/about/SkillCard.tsx
+++ src/components/about/SkillCard.tsx
@@ -11,7 +11,7 @@ export default function SkillCard(props: Props) {
     return (
         <div className={"border border-borders-primary flex flex-col items-center justify-center px-4 p-2 rounded transition-colors duration-150 " + (!header ? " hover:bg-base-accent-primary hover:border-borders-secondary " : "")}>
             <h3>{skill}</h3>
-            <div className="flex flex-row gap-3">
+            <div className="flex flex-row flex-wrap gap-x-3 gap-y-1">
                 <div className="flex flex-row gap-1">
                     <span className="text-base-text-subtlish font-semibold">{Array.from(information.experience).length}</span>
                     <span className="text-base-text-subtle">Relevant Experiences</span>
--- src/components/about/SkillSelector.tsx
+++ src/components/about/SkillSelector.tsx
@@ -2,8 +2,6 @@ import type { SkillInformation } from "../../assets/skills";
 import SkillCard from "../../components/about/SkillCard.tsx";
 import { createSignal } from "solid-js";
 
-type Info = [string, SkillInformation]
-
 function SkillComponent(props: { skills: Record<string, SkillInformation> }) {
     const first_skill = Object.entries(props.skills).at(0);
     if (!first_skill) throw new Error("No skills in object!");
@@ -16,7 +14,7 @@ function SkillComponent(props: { skills: Record<string, SkillInformation> }) {
     return (
         <div className="flex flex-col gap-2">
             {Object.entries(props.skills).filter(([id]) => id == selectedSkill()).map((selected) => {
-                return <div className="grid grid-cols-3 gap-2">
+                return <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-2">
                     <SkillCard skill={selected[0]} information={selected[1]} header={true} />
                     <div>
                         <p>{selected[1].description}</p>
@@ -32,7 +30,7 @@ function SkillComponent(props: { skills: Record<string, SkillInformation> }) {
                     </div>
                 </div>
             })}
-            <div className="grid grid-cols-3 gap-2">
+            <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-2">
                 {Object.entries(props.skills).filter(([id]) => id != selectedSkill()).map(([id, data]) => (
                     <button key={id} onClick={() => handleSkillClick(id)}>
                         <SkillCard skill={id} information={data} header={false} />
