c5b0e7e758f78a0f76beb59f4e68f54db2b3f331
f0rbit <dev@forbit.dev>
2024-03-08 19:10:45 +1030

    add technology map (temporary as devpad doesn't have technology specification)

---
+++ src/assets/technology.ts
@@ -0,0 +1,37 @@
+// map of project-id to list of technologies
+
+
+// first we define all available technologies used in all projects
+export const TECHNOLOGY = {
+    JAVA: "java",
+    GAMEMAKER: "gamemaker",
+    HTML: "html",
+    CSS: "css",
+    JAVASCRIPT: "javascript",
+    GO: "go",
+    TYPESCRIPT: "typescript",
+    CPP: "cpp",
+    RUST: "rust",
+    REACT: "react",
+    SOLIDJS: "solidjs",
+    VUEJS: "vuejs",
+    ASTRO: "astro"
+}
+
+export type TECHNOLOGY = (typeof TECHNOLOGY)[keyof typeof TECHNOLOGY];
+
+export const TECH_MAP = {
+    "gm-server": [TECHNOLOGY.GAMEMAKER, TECHNOLOGY.JAVA],
+    "dungeon-generator": [TECHNOLOGY.JAVA],
+    "file-xxplorer": [TECHNOLOGY.REACT, TECHNOLOGY.RUST, TECHNOLOGY.TYPESCRIPT],
+    "bit-quest": [TECHNOLOGY.GAMEMAKER],
+    "arena": [TECHNOLOGY.GAMEMAKER, TECHNOLOGY.JAVA],
+    "clumsy-santa": [TECHNOLOGY.GAMEMAKER],
+    "pixel-fly": [TECHNOLOGY.GAMEMAKER],
+    "devpad": [TECHNOLOGY.REACT, TECHNOLOGY.TYPESCRIPT],
+    "forbit-dev": [TECHNOLOGY.ASTRO, TECHNOLOGY.SOLIDJS, TECHNOLOGY.TYPESCRIPT],
+    "dev-blog": [TECHNOLOGY.GO, TECHNOLOGY.REACT, TECHNOLOGY.TYPESCRIPT],
+    "comet-events": [TECHNOLOGY.VUEJS, TECHNOLOGY.TYPESCRIPT],
+}
+
+
