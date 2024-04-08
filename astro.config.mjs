8dda8ade7022010e3f8f32281c55824bd0a9c360
f0rbit <dev@forbit.dev>
2024-04-08 21:47:30 +0930

    sitemap & robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import solid from "@astrojs/solid-js";
 import node from "@astrojs/node";
+import { sitemap } from "@astrojs/sitemap";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solid(), icon()],
+  integrations: [tailwind(), solid(), icon(), sitemap()],
   output: "server",
   adapter: node({
     mode: "standalone"
-  })
+  }),
+	site: "https://forbit.dev"
 });
