23cba3a63c7aa606086efe7684cb345dd02c0e4f
f0rbit <dev@forbit.dev>
2024-04-08 21:56:45 +0930

    fix sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import solid from "@astrojs/solid-js";
 import node from "@astrojs/node";
-import { sitemap } from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap";
 
 import icon from "astro-icon";
 
